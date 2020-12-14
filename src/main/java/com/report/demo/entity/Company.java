4ff25b77ec73a1137cc9c67ff9ac0cf609c390a9
Quoc Viet <6908152-viet2k@users.noreply.gitlab.com>
2020-12-14 23:38:22 +0700

    create entity

--- src/main/java/com/report/demo/entity/Company.java
+++ src/main/java/com/report/demo/entity/Company.java
@@ -1,87 +1,87 @@
-//package com.report.demo.entity;
-//
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//import java.sql.Date;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "company")
-//public class Company {
-//
-//    @Id
-//    @GeneratedValue
-//    private int id;
-//    private String company_name;
-//    private String company_hotline;
-//    private String company_address;
-//    private String company_desc;
-//    private Date create_at;
-//    private Date update_at;
-//
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getCompany_name() {
-//        return company_name;
-//    }
-//
-//    public void setCompany_name(String company_name) {
-//        this.company_name = company_name;
-//    }
-//
-//    public String getCompany_hotline() {
-//        return company_hotline;
-//    }
-//
-//    public void setCompany_hotline(String company_hotline) {
-//        this.company_hotline = company_hotline;
-//    }
-//
-//    public String getCompany_address() {
-//        return company_address;
-//    }
-//
-//    public void setCompany_address(String company_address) {
-//        this.company_address = company_address;
-//    }
-//
-//    public String getCompany_desc() {
-//        return company_desc;
-//    }
-//
-//    public void setCompany_desc(String company_desc) {
-//        this.company_desc = company_desc;
-//    }
-//
-//    public Date getCreate_at() {
-//        return create_at;
-//    }
-//
-//    public void setCreate_at(Date create_at) {
-//        this.create_at = create_at;
-//    }
-//
-//    public Date getUpdate_at() {
-//        return update_at;
-//    }
-//
-//    public void setUpdate_at(Date update_at) {
-//        this.update_at = update_at;
-//    }
-//}
+package com.report.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String company_name;
+    private String company_hotline;
+    private String company_address;
+    private String company_desc;
+    private Date create_at;
+    private Date update_at;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getCompany_hotline() {
+        return company_hotline;
+    }
+
+    public void setCompany_hotline(String company_hotline) {
+        this.company_hotline = company_hotline;
+    }
+
+    public String getCompany_address() {
+        return company_address;
+    }
+
+    public void setCompany_address(String company_address) {
+        this.company_address = company_address;
+    }
+
+    public String getCompany_desc() {
+        return company_desc;
+    }
+
+    public void setCompany_desc(String company_desc) {
+        this.company_desc = company_desc;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+}
--- src/main/java/com/report/demo/entity/Post.java
+++ src/main/java/com/report/demo/entity/Post.java
@@ -1,83 +1,83 @@
-//package com.report.demo.entity;
-//
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.sql.Date;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "post")
-//public class Post {
-//
-//    @Id
-//    @GeneratedValue
-//    private int id;
-//    private String post_title;
-//    private String post_desc;
-//    private String company_id;
-//    private String job_id;
-//    private Date create_at;
-//    private Date update_at;
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getPost_title() {
-//        return post_title;
-//    }
-//
-//    public void setPost_title(String post_title) {
-//        this.post_title = post_title;
-//    }
-//
-//    public String getPost_desc() {
-//        return post_desc;
-//    }
-//
-//    public void setPost_desc(String post_desc) {
-//        this.post_desc = post_desc;
-//    }
-//
-//    public String getCompany_id() {
-//        return company_id;
-//    }
-//
-//    public void setCompany_id(String company_id) {
-//        this.company_id = company_id;
-//    }
-//
-//    public String getJob_id() {
-//        return job_id;
-//    }
-//
-//    public void setJob_id(String job_id) {
-//        this.job_id = job_id;
-//    }
-//
-//    public Date getCreate_at() {
-//        return create_at;
-//    }
-//
-//    public void setCreate_at(Date create_at) {
-//        this.create_at = create_at;
-//    }
-//
-//    public Date getUpdate_at() {
-//        return update_at;
-//    }
-//
-//    public void setUpdate_at(Date update_at) {
-//        this.update_at = update_at;
-//    }
-//}
+package com.report.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String post_title;
+    private String post_desc;
+    private String company_id;
+    private String job_id;
+    private Date create_at;
+    private Date update_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPost_title() {
+        return post_title;
+    }
+
+    public void setPost_title(String post_title) {
+        this.post_title = post_title;
+    }
+
+    public String getPost_desc() {
+        return post_desc;
+    }
+
+    public void setPost_desc(String post_desc) {
+        this.post_desc = post_desc;
+    }
+
+    public String getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(String company_id) {
+        this.company_id = company_id;
+    }
+
+    public String getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(String job_id) {
+        this.job_id = job_id;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+}
